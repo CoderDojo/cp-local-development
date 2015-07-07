50839190ec0b4040ff08dd964c5dfb801b5f2723
david-cahill <davidrichardcahill@gmail.com>
2015-07-07 17:11:48 +0100

    make code dry-er

--- system.js
+++ system.js
@@ -73,63 +73,18 @@ module.exports = {
       }];
 
       // add default getter props to all services if not already overridden
-      _.each(services, function (service) {
-        if (!service.repo) {
-          service.__defineGetter__('repo', function() {
-            return baseRepo + service.name;
-          });
-        }
-        var serviceBranch = service.branch;
-        if (!service.branch) {
-          service.__defineGetter__('branch', function() {
-            return serviceBranch || self.systemBranch || defaultBranch;
-          });
-        }
-
-        // most services have the same start command
-        if (!service.start) {
-          service.__defineGetter__('start', function() {
-            return './start.sh empty service.js'
-          });
-        }
-
-        // env function returns the amalgamated environement variables
-        service.__defineGetter__('env', function() {
-          var evars = _.clone(self.env);
-          _.each(service.serviceEnv, function(v,k) {
-            evars[k] = v;
-          });
-
-          // see if user has anything to override
-          try {
-            var localenv = require('./local-env.js');
-            _.each(localenv, function(v,k) {
-              evars[k] = v;
-            });
-          }catch(x) {
-            // purposely ignored
-          }
-          return evars;
-        });
-      });
+      addGetters(services, self);
       return services;
     },
 
     // for easy debugging (the getters defined above to no console.log well!)
     stringify: function() {
-      return {
-        systemBranch: this.systemBranch,
-        services: _.map(this.services, _.toPlainObject)
-      };
+      return stringify(this);
     },
 
     // system env getter, amalgamated with the global env
-    get env () {
-      var evars = _.clone(globalEnv);
-      _.each(this.systemEnv, function(v,k) {
-        evars[k] = v;
-      });
-      return evars;
+    get env () { 
+      return env(this); 
     }
   },
   phase3: {
@@ -207,63 +162,76 @@ module.exports = {
       }];
       
       // add default getter props to all services if not already overridden
-      _.each(services, function (service) {
-        if (!service.repo) {
-          service.__defineGetter__('repo', function() {
-            return baseRepo + service.name;
-          });
-        }
-        var serviceBranch = service.branch;
-        if (!service.branch) {
-          service.__defineGetter__('branch', function() {
-            return serviceBranch || self.systemBranch || defaultBranch;
-          });
-        }
-
-        // most services have the same start command
-        if (!service.start) {
-          service.__defineGetter__('start', function() {
-            return './start.sh empty service.js'
-          });
-        }
-
-        // env function returns the amalgamated environement variables
-        service.__defineGetter__('env', function() {
-          var evars = _.clone(self.env);
-          _.each(service.serviceEnv, function(v,k) {
-            evars[k] = v;
-          });
-
-          // see if user has anything to override
-          try {
-            var localenv = require('./local-env.js');
-            _.each(localenv, function(v,k) {
-              evars[k] = v;
-            });
-          }catch(x) {
-            // purposely ignored
-          }
-          return evars;
-        });
-      });
+      addGetters(services, self);
       return services;
     },
     // for easy debugging (the getters defined above to no console.log well!)
     stringify: function() {
-      return {
-        systemBranch: this.systemBranch,
-        services: _.map(this.services, _.toPlainObject)
-      };
+      return stringify(this);
     },
 
     // system env getter, amalgamated with the global env
-    get env () {
-      var evars = _.clone(globalEnv);
-      _.each(this.systemEnv, function(v,k) {
-        evars[k] = v;
-      });
-      return evars;
+    get env () { 
+      return env(this); 
     }
+  
   }
 
+}
+
+var stringify = function (system) {
+  return {
+    systemBranch: system.systemBranch,
+    services: _.map(system.services, _.toPlainObject)
+  };
+}
+
+var env = function (system) {
+  var evars = _.clone(globalEnv);
+  _.each(system.systemEnv, function(v,k) {
+    evars[k] = v;
+  });
+  return evars;
+}
+
+var addGetters = function (services, self) {
+  _.each(services, function (service) {
+    if (!service.repo) {
+      service.__defineGetter__('repo', function() {
+        return baseRepo + service.name;
+      });
+    }
+    var serviceBranch = service.branch;
+    if (!service.branch) {
+      service.__defineGetter__('branch', function() {
+        return serviceBranch || self.systemBranch || defaultBranch;
+      });
+    }
+
+    // most services have the same start command
+    if (!service.start) {
+      service.__defineGetter__('start', function() {
+        return './start.sh empty service.js'
+      });
+    }
+
+    // env function returns the amalgamated environement variables
+    service.__defineGetter__('env', function() {
+      var evars = _.clone(self.env);
+      _.each(service.serviceEnv, function(v,k) {
+        evars[k] = v;
+      });
+
+      // see if user has anything to override
+      try {
+        var localenv = require('./local-env.js');
+        _.each(localenv, function(v,k) {
+          evars[k] = v;
+        });
+      }catch(x) {
+        // purposely ignored
+      }
+      return evars;
+    });
+  });
 }
