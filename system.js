72d3cfe601a978a85d0971ea4d85a5961799ee06
butlerx <butlerx@notthe.cloud>
2017-07-25 10:11:13 +0100

    variable conflict with function

--- system.js
+++ system.js
@@ -3,32 +3,25 @@ const toPlainObject = require('lodash/toPlainObject');
 const isUndefined = require('lodash/isUndefined');
 
 class Service {
-
   constructor(name, port, host) {
     this.base = `cp-${name}`;
-    this.name = name;
+    this.serviceName = name;
     this.testPort = port;
     this.testHost = host;
   }
 
   get name() {
-    const serviceName = `${this.base}-service`;
-    return serviceName;
+    return `${this.base}-service`;
   }
 
   get database() {
-    const serviceDB = `${this.base}${process.env.ZENTEST === 'true' ? '-test' : '-development'}`;
-    return serviceDB;
-  }
-
-  get base() {
-    return this.base;
+    return `${this.base}${process.env.ZENTEST === 'true' ? '-test' : '-development'}`;
   }
 
   get test() {
     if (isUndefined(this.testPort) || isUndefined(this.testHost)) return undefined;
     return {
-      name: `test-${this.name}-data`,
+      name: `test-${this.serviceName}-data`,
       port: this.testPort,
       host: this.testHost,
     };
