336f21b7e3bdaed9f7abef3007f29b1c2826bcd5
butlerx <butlerx@notthe.cloud>
2017-10-11 15:36:26 +0100

    simplfy docker remove testdata scripts

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,16 +1,35 @@
 #! /usr/bin/env bash
+
+set -e
+
 github="https://github.com/CoderDojo/"
 folder="./workspace-zen"
+
 cd $folder || (echo "Couldn't access $folder" && exit)
-declare -a repos=("cp-badges-service"
-                  "cp-dojos-service"
-                  "cp-eventbrite-service"
-                  "cp-events-service"
-                  "cp-organisations-service"
-                  "cp-translations"
-                  "cp-users-service"
-                  "cp-zen-frontend"
-                  "cp-zen-platform")
-for repo in "${repos[@]}"; do
-  git clone "$github""$repo".git
+git clone "$github"cp-translations.git
+
+declare -a node0=("badges"
+  "dojos"
+  "eventbrite"
+  "events"
+  "users")
+declare -a zen=("frontend"
+  "platform")
+declare -a node8=("organisations")
+
+for repo in "${node0[@]}"; do
+  git clone "$github"cp-"$repo"-service.git
+  docker-compose run --rm --no-deps "$repo" npm install
 done
+
+for repo in "${node8[@]}"; do
+  git clone "$github"cp-"$repo"-service.git
+  docker-compose run --rm --no-deps "$repo" yarn
+done
+
+for repo in "${zen[@]}"; do
+  git clone "$github"cp-zen"$repo".git
+done
+
+docker-compose run --rm --no-deps frontend yarn
+docker-compose run --rm --no-deps zen yarn
