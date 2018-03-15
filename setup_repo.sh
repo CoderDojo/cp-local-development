ba86a85c75cbb2052425c2881105e5a3557c62e9
Daniel Brierton <daniel@brierton.me>
2018-03-15 10:19:28 +0000

    Added new events-service and improved setup_repo script (#73)
    * Added new events-service and improved setup_repo script
    
    * Have event-service start with zen. Also, events2 was a terrible name...

--- setup_repo.sh
+++ setup_repo.sh
@@ -2,22 +2,45 @@
 
 set -e
 
-github="https://github.com/CoderDojo/"
-folder="./workspace-zen"
+base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+github="git@github.com:CoderDojo/"
+# github="https://github.com/CoderDojo/"
+workspace="workspace-zen"
 
-cd $folder || (echo "Couldn't access $folder" && exit)
-git clone "$github"cp-translations.git
+cd "${base_dir}/${workspace}" || (echo "Couldn't access ${base_dir}/${workspace}" && exit)
 
-declare -a services=("badges"
+declare -a legacy_services=("badges"
   "dojos"
   "eventbrite"
   "events"
   "organisations"
   "users")
 
-for repo in "${services[@]}"; do
-  git clone "$github"cp-"$repo"-service.git
+for repo in "${legacy_services[@]}"; do
+  if [ ! -d "${base_dir}/${workspace}/cp-${repo}-service" ]; then
+    git clone "$github"cp-"$repo"-service.git
+  fi
 done
 
-git clone "$github"cp-zen-frontend.git
-git clone "$github"cp-zen-platform.git
+if [ ! -d "${base_dir}/${workspace}/cp-translations" ]; then
+  git clone "$github"cp-translations.git
+fi
+
+if [ ! -d "${base_dir}/${workspace}/cp-zen-frontend" ]; then
+  git clone "$github"cp-zen-frontend.git
+fi
+
+if [ ! -d "${base_dir}/${workspace}/cp-zen-platform" ]; then
+  git clone "$github"cp-zen-platform.git
+fi
+
+declare -a services=("events")
+
+mkdir -p "$base_dir/${workspace}/services"
+cd "${base_dir}/${workspace}/services"
+
+for repo in "${services[@]}"; do
+  if [ ! -d "${base_dir}/${workspace}/services/${repo}-service" ]; then
+    git clone "$github""$repo"-service.git
+  fi
+done
