7fae97b30ea8cc3ad351067b4ac666e77a13b5e9
Glen Pike <github@glenpike.co.uk>
2020-07-03 15:07:05 +0100

    65: Updating Postgres container version to match changes being made to prod.
    Updating Postgres from v9 -> v11 requires a couple of changes around the
    earthdistance extension.  Data can't be reloaded because of a mismatch
    in extension versions so an index needs to be dropped before migration
    and re-instated after.  Future backups should load fine, but currently
    anything we download from S3 can be used with caveats...
    
    - Update Docker container to Postgres 11
    - Add creating of extensions to setup
    - Split untar and restore in the restore script allowing us to work
    with existing directories
    - Add some notes in the restore script
    - Change order so that dojos (which may fail) goes last
    - Remove insert for address column that no longer exists from anon sql

--- cd-db/restore_db.sh
+++ cd-db/restore_db.sh
@@ -2,20 +2,36 @@
 
 set -e
 
-# To restore the a database place the tar.gz in the dump folder
-# they should be called users.tar.gz, dojos.tar.gz and events.tar.gz
+# To restore the a database place an unzipped tar in the dump folder
+# they should be called users.tar, dojos.tar and events.tar
+# or untar a backup of each db into it's corresponding folder users, dojos or events
+# Don't use a gzipped backup as tar will fail
+# FYI Database dumps are stored in an S3 bucket called zen-pg-backup
 
 restore() {
   repo=$1
-  DUMP="/db/${repo}.tar.gz"
+  DUMP="/db/${repo}.tar"
   if [ -f "$DUMP" ]; then
-    echo restoring "$DUMP"
+    echo unpacking "$DUMP"
     mkdir -p /db/"$repo"
     tar xvf "$DUMP" -C /db/"$repo"
+  fi
+  DIR="/db/${repo}/backup_dump"
+  if [ -d "$DIR" ]; then
+    echo restoring "$repo"
     pg_restore -c --if-exists -w -d "cp-${repo}-development" -U platform /db/"$repo"/backup_dump
   fi
 }
 
 restore users
-restore dojos
 restore events
+# dojos may fail when creating the index for nearest_dojos on backups from Posgres 9.4
+# Restart the container and you can run it manually if needed
+# `docker exec -it cp-local-development_db_1 bash`
+# Start a psql console
+# psql -U postgres
+# Change to dojos DB
+# \c cp-dojos-development
+# Execute the SQL
+# CREATE INDEX nearest_dojos ON public.cd_dojos USING gist (public.ll_to_earth((((geo_point -> 'lat'::text))::text)::double precision, (((geo_point -> 'lon'::text))::text)::double precision));
+restore dojos
--- cd-db/setup_zen.sql
+++ cd-db/setup_zen.sql
@@ -1,3 +1,10 @@
+-- Until the SUPERUSER role is removed from dumps
+CREATE ROLE d8jnawwrcw7sp9 SUPERUSER;
+-- END
+-- Dojos DB requires some extensions
+CREATE EXTENSION cube;
+CREATE EXTENSION earthdistance;
+-- END
 create user platform with superuser password 'QdYx3D5y';
 CREATE DATABASE "cp-dojos-development" OWNER platform;
 CREATE DATABASE "cp-users-development" OWNER platform;
